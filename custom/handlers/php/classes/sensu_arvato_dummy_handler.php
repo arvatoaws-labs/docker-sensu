0b2efd0ac46a5fc17861df9335faa9ca84984e88
Patrick Robinson <patrick.robinson@bertelsmann.de>
2020-01-29 14:08:03 +0100

    Add addtional Origin
    Signed-off-by: Patrick Robinson <patrick.robinson@bertelsmann.de>

--- custom/handlers/php/classes/sensu_arvato_dummy_handler.php
+++ custom/handlers/php/classes/sensu_arvato_dummy_handler.php
@@ -3,18 +3,21 @@
 require_once(__DIR__ . '/httpful.phar');
 require_once(__DIR__ . '/sensu_arvato_handler.php');
 
-class SensuArvatoDummyHandler extends SensuArvatoHandler {
+class SensuArvatoDummyHandler extends SensuArvatoHandler
+{
 
-    public function getHandlerName() {
-        return 'dummy';
-    }
+  public function getHandlerName()
+  {
+    return 'dummy';
+  }
 
-    protected function _handleCreate() {
-        $this->log("Handle create", "warn");
-    }
+  protected function _handleCreate()
+  {
+    $this->log("Handle create", "warn");
+  }
 
-    protected function _handleResolve() {
-        $this->log("Handle resolve", "warn");
-    }
-
+  protected function _handleResolve()
+  {
+    $this->log("Handle resolve", "warn");
+  }
 }
